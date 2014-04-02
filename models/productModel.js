3a8d28191b59227357ee590ced9f4d59fe78b390
Matt Edelman <medelman@paypal.com>
2014-04-02 15:29:21 -0400

    modify middleware event listener to act on middleware:after:session, removing extraneous console.log stmts

--- models/productModel.js
+++ models/productModel.js
@@ -4,27 +4,25 @@ var mongoose = require('mongoose');
 
 var productModel = function () {
 
-	//Define a super simple schema for our products.
-	var productSchema = mongoose.Schema({
-		name: String,
-		price: Number
-	});
+    //Define a super simple schema for our products.
+    var productSchema = mongoose.Schema({
+        name: String,
+        price: Number
+    });
 
-	//Verbose toString method
-	productSchema.methods.whatAmI = function () {
-		var greeting = this.name ?
-			'Hello, I\'m a ' + this.name + ' and I\'m worth ' + this.prettyPrice()
-			: 'I don\'t have a name :(';
-		console.log(greeting);
-	};
+    //Verbose toString method
+    productSchema.methods.whatAmI = function () {
+        var greeting = this.name ?
+            'Hello, I\'m a ' + this.name + ' and I\'m worth ' + this.prettyPrice()
+            : 'I don\'t have a name :(';
+    };
 
-	//Format the price of the product to show a dollar sign, and two decimal places
-	productSchema.methods.prettyPrice = function () {
-		//console.log(this);
-		return (this && this.price) ? '$' + this.price.toFixed(2) : '$';
-	};
+    //Format the price of the product to show a dollar sign, and two decimal places
+    productSchema.methods.prettyPrice = function () {
+        return (this && this.price) ? '$' + this.price.toFixed(2) : '$';
+    };
 
-	return mongoose.model('Product', productSchema);
+    return mongoose.model('Product', productSchema);
 
 };
 
