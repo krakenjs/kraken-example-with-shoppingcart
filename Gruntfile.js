947f39116910640ffa04cf58ad9f681e94d56782
Matt Edelman <medelman@paypal.com>
2016-04-28 15:11:25 -0700

    migrate this example to construx middleware

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
     });
 
     // Register group tasks
-    grunt.registerTask('build', [ 'jshint', 'less', 'i18n', 'copyto' ]);
+    grunt.registerTask('build', [ 'jshint', 'less', 'dustjs', 'copyto' ]);
     grunt.registerTask('test', [ 'jshint', 'mochacli' ]);
 
 };
--- tasks/dustjs.js
+++ tasks/dustjs.js
@@ -11,7 +11,7 @@ module.exports = function dustjs(grunt) {
 	        files: [
 	            {
 	                expand: true,
-	                cwd: 'tmp/',
+	                cwd: 'public/templates',
 	                src: '**/*.dust',
 	                dest: '.build/templates',
 	                ext: '.js'
