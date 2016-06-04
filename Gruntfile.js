88c9d791c6096c9696daa57c0790e9d52cde816c
Matt Edelman <medelman@paypal.com>
2016-06-03 17:15:37 -0700

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
     });
 
     // Register group tasks
-    grunt.registerTask('build', [ 'jshint', 'less', 'copyto' ]);
+    grunt.registerTask('build', [ 'jshint', 'less', 'browserify', 'copyto' ]);
     grunt.registerTask('test', [ 'jshint', 'mochacli' ]);
 
 };
