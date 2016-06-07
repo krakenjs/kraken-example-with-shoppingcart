5aed3ee7ea58b10f9c44d33a3d7f831fa566d829
Matt Edelman <medelman@paypal.com>
2016-06-07 10:22:10 -0700

    remove copy/pasta comment

--- public/js/actions.js
+++ public/js/actions.js
@@ -18,28 +18,24 @@ var Constants = require('./constants');
 var Actions = {
 
 	addProduct: function(product) {
-		//update application
 		Dispatcher.dispatch({
 			actionType: Constants.PRODUCT_CREATE,
 			product: product
 		});
 	},
 	deleteProduct: function(product) {
-		//update application
 		Dispatcher.dispatch({
 			actionType: Constants.PRODUCT_DELETE,
 			product: product
 		});
 	},
 	addToCart: function(product) {
-		//update application
 		Dispatcher.dispatch({
 			actionType: Constants.CART_ADD,
 			product: product
 		});
 	},
 	initiatePayment: function(payInfo) {
-		//update application
 		Dispatcher.dispatch({
 			actionType: Constants.PAYMENT_INITIATE,
 			payInfo: payInfo
